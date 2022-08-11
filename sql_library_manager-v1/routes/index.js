c722e57d96e555063f120a9c8810dde3f59ecf9d
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-08-11 02:36:28 -0700

    Create GET & POST routes for /books/new.

--- sql_library_manager-v1/routes/index.js
+++ sql_library_manager-v1/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', async(req, res, next) => {
   res.redirect('/books');
 });
 
-/* GET /books/ - Shows the full list of books */
+/* GET /books - Shows the full list of books */
 router.get('/books', async(req, res, next) => {
   const books = await Book.findAll();
   console.log( books.map(book => book.toJSON()) );
@@ -17,15 +17,16 @@ router.get('/books', async(req, res, next) => {
 
 /* GET /books/new - Shows the create new book form */
 router.get('/books/new', async(req, res, next) => {
- 
-  
+ const books = Book.findAll();
+ res.render('new-book');
 });
 
-/* GET /books/new - Posts a new book to the database */ 
-router.get('/books/new', async(req, res, next) => {
-  
-
+/* POST /books/new - Posts a new book to the database */ 
+router.post('/books/new', async(req, res, next) => {
+ const book = await Book.create(req.body);
+ res.redirect('/books');
 });
+
 /* POST /books/new - Shows book detail form */ 
 router.post('/books/:id', async(req, res, next) => {
  
