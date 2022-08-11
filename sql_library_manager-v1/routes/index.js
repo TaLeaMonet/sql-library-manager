7564d40b0029c629b71416f27c44c4c8e1bdec71
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-08-11 01:58:38 -0700

    Create GET routes for / and /books.

--- sql_library_manager-v1/routes/index.js
+++ sql_library_manager-v1/routes/index.js
@@ -2,11 +2,44 @@ var express = require('express');
 var router = express.Router();
 var Book = require( '../models').Book;
 
-/* GET home page. */
+
+/* GET / - Redirects to /books route. */
 router.get('/', async(req, res, next) => {
+  res.redirect('/books');
+});
+
+/* GET /books/ - Shows the full list of books */
+router.get('/books', async(req, res, next) => {
   const books = await Book.findAll();
-  console.log(books.map(book => book.toJSON()) );
-  res.json(books);
+  console.log( books.map(book => book.toJSON()) );
+  res.render('index', {books, title: "Books"});  
+});
+
+/* GET /books/new - Shows the create new book form */
+router.get('/books/new', async(req, res, next) => {
+ 
+  
+});
+
+/* GET /books/new - Posts a new book to the database */ 
+router.get('/books/new', async(req, res, next) => {
+  
+
+});
+/* POST /books/new - Shows book detail form */ 
+router.post('/books/:id', async(req, res, next) => {
+ 
+
+});
+/* GET /books/:id - Updates book info in the database */ 
+router.get('/books/:id', async(req, res, next) => {
+  
+
+});
+/* POST /books/:id/delete - Deletes a book - Careful, this can't be undone - create test book! */ 
+router.post('/books/:id/delete', async(req, res, next) => {
+  
+
 });
 
 module.exports = router;
